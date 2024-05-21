6a2fda6ee195886980c9df64b4056ef94f71d5c7
GunsNRoses20220917 <122970222+GunsNRoses20220917@users.noreply.github.com>
2024-05-21 19:23:58 +0530

    Add files via upload

---
+++ .devcontainer/0_Basics/00_variables.js
@@ -0,0 +1,18 @@
+// variables are temporary place holders which can keep a value somewhere stored safely in memory.
+// we can either make a variable Permanent meaning Value will not change or we can make it Temporary.
+// there are two types:
+// Let: to define a variable, we should use Let keyword.
+// Const: by using Const keyword, whatever value we assign then it cannot be changed or modified.
+// var: var keyword is also used to define variable. Prefer not to use this because of Block & Functional Scope 
+
+const dateOfBirth = '2nd Feb 1993'
+const myName = "Prakhar Singh Rajawat"
+const myWife = "Tara Rajawat"
+const mySon  = "Samvardhan singh Rajawat"
+
+let myCompany = "ABB India Pvt ltd"
+let myProfile = "Data Modeler"
+let myLocation = "Bengaluru"
+let myCountry = "India"
+
+console.table([dateOfBirth, myName, myWife, mySon, myCompany, myProfile, myLocation, myCountry])
